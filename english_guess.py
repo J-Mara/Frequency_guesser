66072e77e851192d29ba491bbb74084f9a8c06c6
J-Mara <jmaragakis20@stuy.edu>
2022-01-26 15:24:39 -0500

    faster and simple to test

--- english_guess.py
+++ english_guess.py
@@ -2,39 +2,42 @@ import sys
 import math
 import random
 import time
+import urllib.request
 
-password = ""
-password = sys.argv[1]
-lengthp = len(password)
+word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 
-alpha = {
-    'a' : 0,
-    'b' : 0,
-    'c' : 0,
-    'd' : 0,
-    'e' : 0,
-    'f' : 0,
-    'g' : 0,
-    'h' : 0,
-    'i' : 0,
-    'j' : 0,
-    'k' : 0,
-    'l' : 0,
-    'm' : 0,
-    'n' : 0,
-    'o' : 0,
-    'p' : 0,
-    'q' : 0,
-    'r' : 0,
-    's' : 0,
-    't' : 0,
-    'u' : 0,
-    'v' : 0,
-    'w' : 0,
-    'x' : 0,
-    'y' : 0,
-    'z' : 0
-}
+response = urllib.request.urlopen(word_site)
+txt = response.read().decode()
+WORDS = txt.splitlines()
+
+alpha = (
+    'a' ,
+    'b' ,
+    'c' ,
+    'd' ,
+    'e' ,
+    'f' ,
+    'g' ,
+    'h' ,
+    'i' ,
+    'j' ,
+    'k' ,
+    'l' ,
+    'm' ,
+    'n' ,
+    'o' ,
+    'p' ,
+    'q' ,
+    'r' ,
+    's' ,
+    't' ,
+    'u' ,
+    'v' ,
+    'w' ,
+    'x' ,
+    'y' ,
+    'z' 
+)
 
 freq = {
     'e' : 0,
@@ -64,54 +67,108 @@ freq = {
     'q' : 0,
     'z' : 0
 }
-print("here is the password: ")
-print (password)
-print("=======================")
+
+dicFreq = (
+    'e',
+    's',
+    'i',
+    'a',
+    'r',
+    'n',
+    't',
+    'o',
+    'l',
+    'c',
+    'd',
+    'u',
+    'g',
+    'p',
+    'm',
+    'k',
+    'h',
+    'b',
+    'y',
+    'f',
+    'v',
+    'w',
+    'z',
+    'x',
+    'q',
+    'j'
+)
+
 
 def check(attempt, real):
     return (attempt == real)
 
-def sys_guess_lower(x, goal, attempt):
-    if (len(attempt) == x):
-        if(check(attempt, goal)):
-           print(attempt)
-           return True
-        else:
-            return False
-    else:
-        res = False
-        for i in alpha:
-            if(res):
-                return res
-            else:
-                res = sys_guess_lower(x, goal, attempt + i)
-        return False
 
-def sys_guess_lower_order(x, goal, attempt):
-    if (len(attempt) == x):
-        if(check(attempt, goal)):
-            print(attempt)
+def sys_guess_lower(x, goal, attempt, i=0):
+    if(i == x):
+        #return (goal == attempt)
+        if(goal == attempt):
+            #print(attempt)
             return True
-        else:
-            return False
-    else:
-        res = False
-        for i in freq:
-            if(res):
-                return res
-            else:
-                res = sys_guess_lower_order(x, goal, attempt + i)
         return False
+    for n in alpha:
+        if(sys_guess_lower(x, goal, attempt + n, i+1)):
+            return True
+    return False
+
+def sys_guess_lower_order(x, goal, attempt, i=0):
+    if(i == x):
+        #return (goal == attempt)
+        if(goal == attempt):
+            #print(attempt)
+            return True
+        return False
+    for n in dicFreq:
+        if(sys_guess_lower_order(x, goal, attempt + n, i+1)):
+            return True
+    return False
 
 
-print("Standard systematic check:")
-ts1 = time.time()
-print(sys_guess_lower(lengthp, password, ""))
-ts2 = time.time()
-print("it took ",(ts2-ts1)," seconds to run")
-print("===========================")
-print("Ordered by frequency check:")
-ts1 = time.time()
-print(sys_guess_lower_order(lengthp, password, ""))
-ts2 = time.time()
-print("it took ", (ts2-ts1)," seconds to run")
+def random_password_limit(x):
+    word = random.choice(WORDS)
+    if (len(word) <= x):
+        #print (word)
+        return word
+    else:
+        res = ""
+        for char in word:
+            res += char
+            if(len(res) == x):
+                #print (res)
+                return res
+    return False
+
+timeStandard = 0
+timeOrdered = 0
+trials = 50
+
+for i in range(trials):
+    password = random_password_limit(5)
+    lengthp = len(password)
+    #print("here is the password: ")
+    print (password)
+    #print("=======================")
+   # print("Standard systematic check:")
+    ts1 = time.time()
+    sys_guess_lower(lengthp, password, "")
+    ts2 = time.time()
+    #print("it took ",(ts2-ts1)," seconds to run")
+    timeStandard += (ts2-ts1)
+    #print("===========================")
+    #print("Ordered by frequency check:")
+    ts1 = time.time()
+    sys_guess_lower_order(lengthp, password, "")
+    ts2 = time.time()
+    #print("it took ", (ts2-ts1)," seconds to run")
+    timeOrdered += (ts2-ts1)
+
+print()
+print("============")
+print()
+print("Total time standard: ", timeStandard, " seconds")
+print("Total time ordered: ", timeOrdered, " seconds")
+print("differance: ", (timeStandard - timeOrdered), " seconds")
+print("average differance per password: ", (timeStandard-timeOrdered)/trials, " seconds")
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
 default:
 	python3 english_guess.py $(ARGS)
+word:
+	python3 word_creator.py
